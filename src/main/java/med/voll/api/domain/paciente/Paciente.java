5c08e4bc6f7894e120219352ecd2624be33f3e4b
nicolas.carvalho <nicolas.carvalho@cgm.fortaleza.ce.gov.br>
2023-09-26 16:56:50 -0300

    Curso deploy spring

---
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -0,0 +1,21 @@
+package med.voll.api.domain.paciente;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "pacientes")
+@Entity(name = "Paciente")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Paciente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+}
